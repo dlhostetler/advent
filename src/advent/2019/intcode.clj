012468d47b08d0ad6d9d5911e63c92ae692f105f
d-hostetler <dlhostet@gmail.com>
2019-12-05 01:21:39 -0500

    2019, Day 5, Part 2.

--- src/advent/2019/intcode.clj
+++ src/advent/2019/intcode.clj
@@ -5,14 +5,11 @@
    2 :multiply
    3 :input
    4 :output
+   5 :jump-if-true
+   6 :jump-if-false
+   7 :less-than
+   8 :equals
    99 :halt})
-
-(def num-instruction-vals
-  {:add 4
-   :halt 0
-   :input 2
-   :output 2
-   :multiply 4})
 
 (defn digits [n]
   (->> n str (map (comp read-string str))))
@@ -24,31 +21,28 @@
       (into v (repeat n pad-val))
       v)))
 
-(defn- current-instruction [memory pointer]
+(defn- current-instruction [{:keys [memory pointer]}]
   (-> (nth memory pointer)
       (mod 100)
       instruction->keyword))
 
-(defn- advance-pointer [memory pointer]
-  (let [instruction (current-instruction memory pointer)]
-    (cond
-      ;; the halt instruction is special in that it moves the pointer to indicate
-      ;; stoppage
-      (= :halt instruction)
-      -1
-      instruction
-      (+ pointer (num-instruction-vals instruction))
-      :else
-      1)))
+(defn- advance-pointer [state args]
+  (update state :pointer + (inc (count args))))
 
-(defn- deref-param [memory pointer]
-  (let [memory-index (nth memory pointer)]
+(defn- set-pointer [state pointer]
+  (assoc state :pointer pointer))
+
+(defn- halt-pointer [state]
+  (set-pointer state -1))
+
+(defn- deref-param [memory i]
+  (let [memory-index (nth memory i)]
     (nth memory memory-index)))
 
 (defn safe-assoc [v i val]
   (assoc (pad v i 0) i val))
 
-(defn- parse-args [memory pointer args]
+(defn- parse-args [{:keys [memory pointer]} args]
   (let [position-modes (-> (nth memory pointer)
                            (/ 100)
                            int
@@ -72,45 +66,88 @@
 
 (defmulti execute-instruction current-instruction)
 
-(defmethod execute-instruction :add [memory pointer]
-  (let [[arg0 arg1 out-pos] (parse-args memory pointer [:in :in :out])]
+(defmethod execute-instruction :add [{:keys [pointer] :as state}]
+  (let [args [:in :in :out]
+        [arg0 arg1 out-pos] (parse-args state args)]
     (println (str "(" pointer ")") "Executing add of" arg0 arg1 "into" out-pos)
-    (safe-assoc memory out-pos (+ arg0 arg1))))
+    (-> state
+        (update :memory safe-assoc out-pos (+ arg0 arg1))
+        (advance-pointer args))))
 
-(defmethod execute-instruction :halt [memory pointer]
+(defmethod execute-instruction :equals [{:keys [pointer] :as state}]
+  (let [args [:in :in :out]
+        [arg0 arg1 out-pos] (parse-args state args)]
+    (println (str "(" pointer ")") "Executing equals of" arg0 arg1 "into" out-pos)
+    (let [val (if (= arg0 arg1) 1 0)]
+      (-> state
+          (update :memory safe-assoc out-pos val)
+          (advance-pointer args)))))
+
+(defmethod execute-instruction :halt [{:keys [pointer] :as state}]
   (println (str "(" pointer ")") "Executing halt operation (no op).")
-  memory)
+  (halt-pointer state))
 
-(defmethod execute-instruction :input [memory pointer]
-  (let [[out-pos] (parse-args memory pointer [:out])]
+(defmethod execute-instruction :input [{:keys [pointer] :as state}]
+  (let [args [:out]
+        [out-pos] (parse-args state args)]
     (println (str "(" pointer ")") "Executing input of" out-pos)
     (print "> ")
     (flush)
     (let [i (Integer/parseInt (read-line))]
-      (safe-assoc memory out-pos i))))
+      (-> state
+          (update :memory safe-assoc out-pos i)
+          (advance-pointer args)))))
 
-(defmethod execute-instruction :multiply [memory pointer]
-  (let [[arg0 arg1 out-pos] (parse-args memory pointer [:in :in :out])]
+(defmethod execute-instruction :jump-if-false [{:keys [pointer] :as state}]
+  (let [args [:in :in]
+        [arg0 arg1] (parse-args state args)]
+    (println (str "(" pointer ")") "Executing jump if false for" arg0 arg1)
+    (if (zero? arg0)
+      (set-pointer state arg1)
+      (advance-pointer state args))))
+
+(defmethod execute-instruction :jump-if-true [{:keys [pointer] :as state}]
+  (let [args [:in :in]
+        [arg0 arg1] (parse-args state args)]
+    (println (str "(" pointer ")") "Executing jump if true for" arg0 arg1)
+    (if-not (zero? arg0)
+      (set-pointer state arg1)
+      (advance-pointer state args))))
+
+(defmethod execute-instruction :less-than [{:keys [pointer] :as state}]
+  (let [args [:in :in :out]
+        [arg0 arg1 out-pos] (parse-args state args)]
+    (println (str "(" pointer ")") "Executing less than of" arg0 arg1 "into" out-pos)
+    (let [val (if (< arg0 arg1) 1 0)]
+      (-> state
+          (update :memory safe-assoc out-pos val)
+          (advance-pointer args)))))
+
+(defmethod execute-instruction :multiply [{:keys [memory pointer] :as state}]
+  (let [args [:in :in :out]
+        [arg0 arg1 out-pos] (parse-args state args)]
     (println (str "(" pointer ")")
              "Executing multiply of" arg0 arg1 "into" out-pos)
-    (safe-assoc memory out-pos (* arg0 arg1))))
+    (-> state
+        (update :memory safe-assoc out-pos (* arg0 arg1))
+        (advance-pointer args))))
 
-(defmethod execute-instruction :output [memory pointer]
-  (let [[arg0] (parse-args memory pointer [:out])]
+(defmethod execute-instruction :output [{:keys [memory pointer] :as state}]
+  (let [args [:out]
+        [arg0] (parse-args state args)]
     (println (str "(" pointer ")") "Executing output of" arg0)
     (println (get memory arg0))
-    memory))
+    (advance-pointer state args)))
 
 ;; Public
 ;; ======
 
 (defn execute-instructions [memory]
-  (loop [next memory
-         pointer 0]
-    (if-not (neg? pointer)
-      (recur (execute-instruction next pointer)
-             (advance-pointer next pointer))
-      next)))
+  (loop [next {:memory memory
+               :pointer 0}]
+    (if-not (-> next :pointer neg?)
+      (recur (execute-instruction next))
+      (:memory next))))
 
 (defn set-input [memory noun verb]
   (-> memory
