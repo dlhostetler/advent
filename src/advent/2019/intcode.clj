236350723e39c6110d7b0a8ed872a9e84e9f3864
d-hostetler <dlhostet@gmail.com>
2019-12-05 00:53:12 -0500

    2019, Day 5, Part 1.

--- src/advent/2019/intcode.clj
+++ src/advent/2019/intcode.clj
@@ -3,54 +3,103 @@
 (def instruction->keyword
   {1 :add
    2 :multiply
+   3 :input
+   4 :output
    99 :halt})
 
 (def num-instruction-vals
   {:add 4
    :halt 0
+   :input 2
+   :output 2
    :multiply 4})
 
+(defn digits [n]
+  (->> n str (map (comp read-string str))))
+
+(defn pad [v length pad-val]
+  (let [n (- length (count v))]
+    (if (pos? n)
+      ;; need to extend the array
+      (into v (repeat n pad-val))
+      v)))
+
 (defn- current-instruction [memory pointer]
-  (instruction->keyword (nth memory pointer)))
+  (-> (nth memory pointer)
+      (mod 100)
+      instruction->keyword))
 
 (defn- advance-pointer [memory pointer]
   (let [instruction (current-instruction memory pointer)]
-    (if (= :halt instruction)
+    (cond
       ;; the halt instruction is special in that it moves the pointer to indicate
       ;; stoppage
+      (= :halt instruction)
       -1
-      (+ pointer (num-instruction-vals instruction)))))
+      instruction
+      (+ pointer (num-instruction-vals instruction))
+      :else
+      1)))
 
 (defn- deref-param [memory pointer]
   (let [memory-index (nth memory pointer)]
     (nth memory memory-index)))
 
-(defn- inputs [memory pointer num-inputs]
-  (into [] (for [offset (range num-inputs)]
-             (deref-param memory (+ pointer offset 1)))))
+(defn safe-assoc [v i val]
+  (assoc (pad v i 0) i val))
 
-(defn output-positions [memory pointer num-inputs num-outputs]
-  (into [] (for [offset (range num-outputs)]
-             (nth memory (+ pointer num-inputs offset 1)))))
+(defn- parse-args [memory pointer args]
+  (let [position-modes (-> (nth memory pointer)
+                           (/ 100)
+                           int
+                           digits
+                           reverse
+                           vec
+                           (pad (count args) 0))]
+    (into [] (for [i (range (count args))
+                   :let [arg-type (nth args i)
+                         offset (+ pointer i 1)
+                         position-mode (nth position-modes i)]]
+
+               (cond
+                 (and (= :in arg-type) (= 0 position-mode))
+                 (deref-param memory offset)
+                 (or (= :out arg-type) (= 1 position-mode))
+                 (nth memory offset)
+                 :else
+                 (throw (ex-info "Unknown position mode."
+                                 {:position-mode position-mode})))))))
 
 (defmulti execute-instruction current-instruction)
 
 (defmethod execute-instruction :add [memory pointer]
-  (let [[arg0 arg1] (inputs memory pointer 2)
-        [out-pos] (output-positions memory pointer 2 1)]
+  (let [[arg0 arg1 out-pos] (parse-args memory pointer [:in :in :out])]
     (println (str "(" pointer ")") "Executing add of" arg0 arg1 "into" out-pos)
-    (assoc memory out-pos (+ arg0 arg1))))
+    (safe-assoc memory out-pos (+ arg0 arg1))))
 
 (defmethod execute-instruction :halt [memory pointer]
   (println (str "(" pointer ")") "Executing halt operation (no op).")
   memory)
 
+(defmethod execute-instruction :input [memory pointer]
+  (let [[out-pos] (parse-args memory pointer [:out])]
+    (println (str "(" pointer ")") "Executing input of" out-pos)
+    (print "> ")
+    (flush)
+    (let [i (Integer/parseInt (read-line))]
+      (safe-assoc memory out-pos i))))
+
 (defmethod execute-instruction :multiply [memory pointer]
-  (let [[arg0 arg1] (inputs memory pointer 2)
-        [out-pos] (output-positions memory pointer 2 1)]
+  (let [[arg0 arg1 out-pos] (parse-args memory pointer [:in :in :out])]
     (println (str "(" pointer ")")
              "Executing multiply of" arg0 arg1 "into" out-pos)
-    (assoc memory out-pos (* arg0 arg1))))
+    (safe-assoc memory out-pos (* arg0 arg1))))
+
+(defmethod execute-instruction :output [memory pointer]
+  (let [[arg0] (parse-args memory pointer [:out])]
+    (println (str "(" pointer ")") "Executing output of" arg0)
+    (println (get memory arg0))
+    memory))
 
 ;; Public
 ;; ======
