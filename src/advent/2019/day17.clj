7f7978d2035a4b5f71dafaf33b63d35351c09a15
d-hostetler <dlhostet@gmail.com>
2019-12-17 02:19:23 -0500

    2019, Day 17, Part 2.

--- src/advent/2019/day17.clj
+++ src/advent/2019/day17.clj
@@ -2,70 +2,47 @@
   (:require [advent.2019.intcode :as intcode]
             [advent.2019.grid :as grid]))
 
-(defn all-10s? [s]
-  (every? #(= 10 %) s))
+(defn comma-separate [input]
+  (interpose \, input))
 
-(defn output->grid [output]
-  (loop [rows (->> output
-                   (partition-by #(= 10 %))
-                   (remove all-10s?))
-         grid {}
-         y 0]
-    (if-not (empty? rows)
-      (recur (rest rows)
-             (reduce (fn [g [x ascii]]
-                       (assoc g [x y] ascii))
-                     grid
-                     (map-indexed (fn [i ascii] [i ascii]) (first rows)))
-             (inc y))
-      grid)))
+(defn asciify-one [x]
+  (cond
+    (= \n x)
+    10
+    (char? x)
+    (int x)
+    (int? x)
+    (->> x str seq (map int))
+    :else
+    (throw (ex-info "Not sure what x is." {:x x}))))
 
-(defn visualize-ascii [ascii]
-  (str (char ascii)))
-
-(defn east [position]
-  (update position 0 inc))
-
-(defn north [position]
-  (update position 1 inc))
-
-(defn south [position]
-  (update position 1 dec))
-
-(defn west [position]
-  (update position 0 dec))
-
-(defn scaffold? [ascii]
-  (when ascii (= 35 ascii)))
-
-(defn intersection? [grid position]
-  (let [adjoining [(east position)
-                   (north position)
-                   (south position)
-                   (west position)]
-        num-scaffolds (->> adjoining
-                           (map grid)
-                           (filter scaffold?)
-                           count)]
-    (>= num-scaffolds 3)))
+(defn asciify [inputs]
+  (->> inputs
+       (map comma-separate)
+       (#(interleave % (repeat [\n])))
+       flatten
+       (map asciify-one)
+       flatten
+       (into [])))
 
 (defn run []
   (let [memory (intcode/file->memory "resources/day17.input")
-        output (atom [])]
+        in (atom (asciify [[\A \B \A \C \B \A \C \B \A \C]
+                           #_A [\L 12 \L 12 \L 6 \L 6]
+                           #_B [\R 8 \R 4 \L 12]
+                           #_C [\L 12 \L 6 \R 12 \R 8]
+                           [\y]]))
+        in (fn []
+             (let [next (-> in deref first)]
+               (swap! in rest)
+               next))
+        output (fn [x]
+                 (if (= 10 x)
+                   (println)
+                   (print (char x))))]
     (intcode/execute-instructions :robot
-                                  memory
-                                  intcode/stdin
-                                  #(swap! output conj %)
-                                  nil)
-    (let [grid (output->grid @output)
-          intersections (->> grid
-                             (filter (comp scaffold? val))
-                             keys
-                             (filter #(intersection? grid %)))]
-      (grid/print (reduce (fn [g point] (assoc g point 79)) grid intersections)
-                  visualize-ascii
-                  {:default 124
-                   :empty-point "?"
-                   :padding 0
-                   :y-dir :top-down})
-      (reduce (fn [acc [x y]] (+ acc (* x y))) 0 intersections))))
+                                    memory
+                                    in
+                                    output
+                                    nil)
+    nil))
