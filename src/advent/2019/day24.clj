4ea058e9f6274abe368f7f777dd6419320349e84
d-hostetler <dlhostet@gmail.com>
2019-12-24 09:08:46 -0500

    2019, Day 24, Part 2.

--- src/advent/2019/day24.clj
+++ src/advent/2019/day24.clj
@@ -5,14 +5,9 @@
 (def width 5)
 (def height 5)
 
-(def all-points
-  (into [] (for [x (range width)
-                 y (range height)]
-             [x y])))
-
 (defn parse-bug [x y c]
   (when (= \# c)
-    [x y]))
+    [x y 0]))
 
 (defn parse-row [y row]
   (map-indexed #(parse-bug %1 y %2) row))
@@ -28,7 +23,7 @@
        (into #{})))
 
 (defn display [bugs]
-  (let [grid (into {} (zipmap bugs (repeat "#")))]
+  (let [grid (into {} (zipmap (mapv butlast bugs) (repeat "#")))]
     (grid/print grid
                 (fn [tile]
                   (when tile tile))
@@ -39,8 +34,18 @@
                  :min-x 0
                  :min-y 0
                  :padding 0
-                 :y-dir :top-down})
-    (println))
+                 :y-dir :top-down}))
+  (println)
+  bugs)
+
+(defn display-all [bugs]
+  (doseq [level (->> bugs
+                     (map last)
+                     (into #{})
+                     sort)]
+    (println (str "Level " level ":"))
+    (display (filter #(= level (last %)) bugs)))
+  (println "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
   bugs)
 
 (defn east [position]
@@ -55,11 +60,67 @@
 (defn west [position]
   (update position 0 dec))
 
+(def center [2 2])
+(def center-w (west center))
+(def center-e (east center))
+(def center-n (north center))
+(def center-s (south center))
+
+(defn adjust-outer-edge [[x y level :as neighbor]]
+  (cond
+    ;; left of area
+    (neg? x)
+    (conj center-w (dec level))
+    ;; right of area
+    (>= x width)
+    (conj center-e (dec level))
+    ;; top of area
+    (neg? y)
+    (conj center-n (dec level))
+    ;; bottom of area
+    (>= y height)
+    (conj center-s (dec level))
+    ;; in area
+    :else
+    neighbor))
+
+(defn adjust-inner-edge
+  [[point-x point-y level :as point] [neighbor-x neighbor-y :as neighbor]]
+  (if (= [neighbor-x neighbor-y] center)
+    (cond
+      ;; center left
+      (= center-w [point-x point-y])
+      (into [] (for [neighbor-y (range height)]
+                 [0 neighbor-y (inc level)]))
+      ;; center right
+      (= center-e [point-x point-y])
+      (into [] (for [neighbor-y (range height)]
+                 [(dec width) neighbor-y (inc level)]))
+      ;; center top
+      (= center-n [point-x point-y])
+      (into [] (for [neighbor-x (range width)]
+                 [neighbor-x 0 (inc level)]))
+      ;; center bottom
+      (= center-s [point-x point-y])
+      (into [] (for [neighbor-x (range width)]
+                 [neighbor-x (dec height) (inc level)]))
+      :else
+      (throw (ex-info "Unknown neighbor to center." {:neighbor neighbor
+                                                     :point point})))
+    [neighbor]))
+
 (defn point->neighbors [point]
-  [(east point)
-   (north point)
-   (south point)
-   (west point)])
+  (->> [(east point)
+        (north point)
+        (south point)
+        (west point)]
+       (map adjust-outer-edge)
+       (mapcat #(adjust-inner-edge point %))))
+
+(alter-var-root #'point->neighbors memoize)
+
+(defn bugs->points [bugs]
+  (into bugs (mapcat point->neighbors bugs)))
 
 (defn life [bugs next-bugs point]
   (let [bug? (bugs point)
@@ -78,21 +139,15 @@
       next-bugs)))
 
 (defn minute [bugs]
-  (reduce #(life bugs %1 %2) #{} all-points))
-
-(defn point->biodiversity [[x y]]
-  (let [n (+ (* y height) x)]
-    (long (Math/pow 2 n))))
+  (reduce #(life bugs %1 %2) #{} (bugs->points bugs)))
 
 (defn run []
   (->> (loop [bugs (parse-area)
-              seen #{}]
-         (if-not (seen bugs)
+              n 200]
+         (if (pos? n)
            (do
-             (display bugs)
-             (recur (minute bugs)
-                    (conj seen bugs)))
+             (display-all bugs)
+             (recur (minute bugs) (dec n)))
            bugs))
-       display
-       (map point->biodiversity)
-       (reduce + 0)))
+       display-all
+       count))
