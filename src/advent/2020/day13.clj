d723cafcba7f69be321dfb50313b6839e9ccb30a
Dennis Hostetler <dlhostet@gmail.com>
2020-12-13 01:49:40 -0500

    2020, Day 13, Part 2.

--- src/advent/2020/day13.clj
+++ src/advent/2020/day13.clj
@@ -1,28 +1,24 @@
 (ns advent.2020.day13
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [plumbing.core :refer :all]))
 
 (def buses-raw "17,x,x,x,x,x,x,x,x,x,x,37,x,x,x,x,x,571,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,13,x,x,x,x,23,x,x,x,x,x,29,x,401,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,19")
 
-(defn buses []
+(defn parse-buses []
   (->> (str/split buses-raw #",")
-       (remove (partial = "x"))
-       (map #(Integer/parseInt %))))
+       (map-indexed vector)
+       (into {})
+       (remove (comp (partial = "x") val))
+       (map-vals #(Integer/parseInt %))))
 
-(defn bus-after [arrival bus-id]
-  [(-> (/ arrival bus-id)
-       int
-       inc
-       (* bus-id))
-   bus-id])
+(defn minutes-left [minValue offset bus-id]
+  (mod (+ minValue offset) bus-id))
 
 (defn run []
-  (let [my-arrival 1000655
-        id-by-arrival (->> (buses)
-                           (map (partial bus-after my-arrival))
-                           (into {}))
-        target-arrival (->> id-by-arrival
-                            (map key)
-                            (apply min))
-        bus-id (get id-by-arrival target-arrival)]
-    (* (- target-arrival my-arrival)
-       bus-id)))
+  (let [start-time (atom 0)
+        product (atom 1)]
+    (doseq [[bus-index bus-id] (parse-buses)]
+      (while (not (zero? (minutes-left @start-time bus-index bus-id)))
+        (swap! start-time + @product))
+      (swap! product * bus-id))
+    @start-time))
