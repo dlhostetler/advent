a13e3a2d7a6761bd3bb9d42a9c8985b58e603ce0
Dennis Hostetler <dlhostet@gmail.com>
2020-12-22 02:46:51 -0500

    2020, Day 22, Part 2.

--- src/advent/2020/day22.clj
+++ src/advent/2020/day22.clj
@@ -1,6 +1,5 @@
 (ns advent.2020.day22
-  (:require [advent.seq :as seq]
-            [plumbing.core :refer :all]))
+  (:require [plumbing.core :refer :all]))
 
 (def player1 [10
               39
@@ -26,7 +25,8 @@
               3
               31
               14
-              4])
+              4
+              ])
 
 (def player2 [2
               15
@@ -54,31 +54,58 @@
               28
               43])
 
-(defn next-state [{:keys [player1 player2] :as state}]
-  (let [card1 (first player1)
-        card2 (first player2)]
-    (if (and card1 card2)
-      (if (> card1 card2)
-        {:player1 (into (-> player1 rest vec)
-                        [card1 card2])
-         :player2 (-> player2 rest vec)}
-        {:player1 (-> player1 rest vec)
-         :player2 (into (-> player2 rest vec)
-                        [card2 card1])})
-      state)))
+(defn cards->winner [card1 card2]
+  (if (> card1 card2) :player1 :player2))
 
-(defn done? [{:keys [player1 player2]}]
-  (or (empty? player1)
-      (empty? player2)))
+(defn state->winner [{:keys [player2]}]
+  (if (empty? player2) :player1 :player2))
 
 (defn score [{:keys [player1 player2]}]
   (let [cards (if (empty? player1) player2 player1)]
     (->> (map * cards (-> cards count inc range rest reverse))
          (reduce +))))
 
+(defn already-seen? [{:keys [player1 player2 rounds]}]
+  (contains? rounds [player1 player2]))
+
+(defn done? [{:keys [player1 player2]}]
+  (or (empty? player1)
+      (empty? player2)))
+
+(defn finish-round [next-state card1 card2 winner]
+  (if (= winner :player1)
+    (update next-state :player1 concat [card1 card2])
+    (update next-state :player2 concat [card2 card1])))
+
+(defn round [{:keys [player1 player2] :as state}]
+  (let [[card1 & next-player1] player1
+        [card2 & next-player2] player2
+        next-state (-> state
+                       (assoc :player1 next-player1)
+                       (assoc :player2 next-player2)
+                       (update :rounds (fnil conj #{}) [player1 player2]))]
+    (cond
+      (already-seen? state)
+      (assoc next-state :player2 [])
+
+      (done? state)
+      state
+
+      (or (< (count next-player1) card1)
+          (< (count next-player2) card2))
+      (-> next-state
+          (finish-round card1 card2 (cards->winner card1 card2))
+          recur)
+
+      :else
+      (let [winner (-> {:player1 (take card1 next-player1)
+                        :player2 (take card2 next-player2)}
+                       round
+                       state->winner)]
+        (-> next-state
+            (finish-round card1 card2 winner)
+            recur)))))
+
 (defn run []
-  (->> (seq/successive next-state {:player1 player1
-                                   :player2 player2})
-       (filter done?)
-       first
-       score))
+  (score (round {:player1 player1
+                 :player2 player2})))
