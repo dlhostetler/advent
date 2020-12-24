7ca6a1f4bf78e74ffd1fab07a9d4d7ea627fa6e0
Dennis Hostetler <dlhostet@gmail.com>
2020-12-24 01:00:02 -0500

    2020, Day 24, Part 2.

--- src/advent/2020/day24.clj
+++ src/advent/2020/day24.clj
@@ -1,5 +1,6 @@
 (ns advent.2020.day24
-  (:require [clojure.java.io :as io]
+  (:require [advent.seq :as seq]
+            [clojure.java.io :as io]
             [plumbing.core :refer :all]))
 
 (def dir->offsets
@@ -9,6 +10,7 @@
    :sw [0 -1]
    :w [-1 0]
    :nw [-1 1]})
+(def offsets (vals dir->offsets))
 
 (defn parse-directions [line]
   (->> line
@@ -39,10 +41,62 @@
 (defn flip-at [tiles position]
   (update tiles position flip))
 
-(defn run []
+(defn color-at [tiles position]
+  (get tiles position :white))
+
+(defn neighbors [position]
+  (map #(next-tile position %) offsets))
+
+(defn neighbor-colors [tiles position]
+  (->> position
+       neighbors
+       (map #(color-at tiles %))))
+
+(defn next-day-tile [tiles position]
+  (let [color (color-at tiles position)
+        num-black-neighbors (->> position
+                                 (neighbor-colors tiles)
+                                 (filter (partial = :black))
+                                 count)]
+    (cond
+      ;; Any black tile with zero or more than 2 black tiles immediately
+      ;; adjacent to it is flipped to white.
+      (and (= color :black)
+           (or (zero? num-black-neighbors)
+               (> num-black-neighbors 2)))
+      :white
+      ;; Any white tile with exactly 2 black tiles immediately adjacent to it is
+      ;; flipped to black.
+      (and (= color :white)
+           (= num-black-neighbors 2))
+      :black
+      ;; Otherwise, the seat's state does not change.
+      :else
+      color)))
+
+(defn next-day [tiles]
+  (->> (for [position (->> tiles
+                           keys
+                           (mapcat neighbors)
+                           (into (-> tiles keys set)))]
+         [position (next-day-tile tiles position)])
+       (remove #(-> % last (= :white)))
+       (into {})))
+
+(defn init-state []
   (->> (tiles)
        (map traverse)
-       (reduce flip-at {})
+       (reduce flip-at {})))
+
+(defn num-black [tiles]
+  (->> tiles
        vals
        (filter (partial = :black))
        count))
+
+(defn run []
+  (->> (init-state)
+       (seq/successive next-day)
+       (take 101)
+       last
+       num-black))
