9d2927b0b4bc1d595c07f3a95b27969821900a7e
Dennis Hostetler <dlhostet@gmail.com>
2021-12-06 00:19:07 -0500

    2021, Day 6, Part 1.

---
+++ src/advent/2021/day6.clj
@@ -0,0 +1,35 @@
+(ns advent.2021.day6
+  (:require [advent.seq :as seq]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [plumbing.core :refer :all]))
+
+(defn fishes []
+  (->> (-> "resources/2021/day6.input"
+           io/reader
+           slurp
+           (str/split #","))
+       (map #(Integer/parseInt %))
+       (group-by identity)
+       (map-vals count)))
+
+(defn next-fishes [fishes [countdown num]]
+  (if (zero? countdown)
+    (-> fishes
+        (update 8 (fnil + 0) num)
+        (update 6 (fnil + 0) num))
+    (update fishes
+            (dec countdown)
+            (fnil + 0)
+            num)))
+
+(defn next-fisheses [fishes]
+  (reduce next-fishes {} fishes))
+
+(defn run []
+  (->> (fishes)
+       (seq/successive next-fisheses)
+       (drop 80)
+       first
+       vals
+       (reduce +)))
