aca57c8a783211003de6294da2ed46244e58e2d4
Dennis Hostetler <dlhostet@gmail.com>
2021-12-14 01:40:24 -0500

    2021, Day 14, Part 2.

--- src/advent/2021/day14.clj
+++ src/advent/2021/day14.clj
@@ -4,12 +4,18 @@
             [clojure.string :as str]
             [plumbing.core :refer :all]))
 
-(def polymer-template
-  (->> "NBOKHVHOSVKSSBSVVBCS"
-       seq
-       (mapv str)))
+(def polymer-template-str "NBOKHVHOSVKSSBSVVBCS")
 
-(defn parse-pair->insertion [s]
+(def polymer-template
+  (->> polymer-template-str
+       seq
+       (mapv str)
+       (partition 2 1)
+       (map vec)
+       (map (fn [pair] [pair 1]))
+       (reduce (fn [m [k v]] (update m k (fnil + 0) v)) {})))
+
+(defn split-rule [s]
   (str/split s #" -> "))
 
 (defn parse-pair [pairStr]
@@ -17,25 +23,24 @@
        seq
        (mapv str)))
 
-(defn create-next [[[a :as pair] insertion]]
-  [pair [a insertion]])
-
-(def pair->next
+(def rules
   (->> "resources/2021/day14.input"
        io/reader
        line-seq
-       (map parse-pair->insertion)
-       (map-keys parse-pair)
-       (map create-next)
-       (into {})))
+       (map split-rule)
+       (map-keys parse-pair)))
+
+(defn next-elements [polymer [[a b :as pair] count]]
+  (let [element (get rules pair)]
+    (-> polymer
+        (update [a element] (fnil + 0) count)
+        (update [element b] (fnil + 0) count))))
 
 (defn next-step [polymer]
-  (-> (->> polymer
-           (partition 2 1)
-           (map pair->next)
-           flatten
-           vec)
-      (conj (last polymer))))
+  (reduce next-elements {} polymer))
+
+(defn count-element [element-counts [[a _] count]]
+  (update element-counts a (fnil + 0) count))
 
 (defn max-element [min-element-pair element-pair]
   (if (> (val element-pair) (val min-element-pair))
@@ -50,11 +55,11 @@
 (defn run []
   (let [polymer (->> polymer-template
                      (seq/successive next-step)
-                     (drop 10)
+                     (drop 40)
                      first)
-        element-counts (->> polymer
-                            (group-by identity)
-                            (map-vals count))
+        element-counts (reduce count-element
+                               {(-> polymer-template-str last str) 1}
+                               polymer)
         least-common (reduce min-element element-counts)
         most-common (reduce max-element element-counts)]
     (- (val most-common) (val least-common))))
