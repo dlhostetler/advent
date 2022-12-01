f6d2bec06aa934efec12e8bdff8baa49b1d00fe6
Dennis Hostetler <dlhostet@gmail.com>
2022-12-01 00:05:41 -0500

    2022, Day 1, Part 1.

---
+++ src/advent/2022/day1.clj
@@ -0,0 +1,18 @@
+(ns advent.2022.day1
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(def input
+  (-> "resources/2022/day1.input"
+      io/reader
+      slurp))
+
+(defn parse-inventory [s]
+  (->> (str/split s #"\n")
+      (map #(Integer/parseInt %))))
+
+(defn run []
+  (->> (str/split input #"\n\n")
+       (map parse-inventory)
+       (map #(reduce + %))
+       (reduce max)))
