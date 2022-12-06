fd66e5785e428702b430fcafb50720f767e41829
Dennis Hostetler <dlhostet@gmail.com>
2022-12-06 00:11:32 -0500

    2022, Day 6, Part 1.

---
+++ src/advent/2022/day6.clj
@@ -0,0 +1,18 @@
+(ns advent.2022.day6
+  (:require [plumbing.core :refer :all]))
+
+(def marker-length 4)
+
+(def input
+  (slurp "resources/2022/day6.input"))
+
+(defn start-of-packet? [characters]
+  (= (-> characters set count) marker-length))
+
+(defn run []
+  (->> input
+       seq
+       (partition marker-length 1)
+       (take-while (comp not start-of-packet?))
+       count
+       (+ marker-length)))
