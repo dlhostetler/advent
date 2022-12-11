aaedfcb77056d909a5b00ec3abe0afd03abf2431
Dennis Hostetler <dlhostet@gmail.com>
2022-12-11 01:28:15 -0500

    2022, Day 11, Part 2.

--- src/advent/2022/day11.clj
+++ src/advent/2022/day11.clj
@@ -1,6 +1,5 @@
 (ns advent.2022.day11
-  (:require [advent.seq :as seq]
-            [clojure.java.io :as io]
+  (:require [clojure.java.io :as io]
             [clojure.string :as str]
             [plumbing.core :refer :all]))
 
@@ -11,40 +10,43 @@
   (let [[_ op-str operand-str] (re-matches #".+new = old ([+*]) (\w+)"
                                            operation)
         op (if (= op-str "+") + *)
-        n (when (str-is-number? operand-str) (Integer/parseInt operand-str))]
+        n (when (str-is-number? operand-str)
+            (bigint operand-str))]
     (if n
       (fn [old]
         (op old n))
-      (fn [old] (op old old)))))
+      (fn [old]
+        (op old old)))))
 
-(defn new-next-monkey [test-str tests-true-str tests-false-str]
-  (let [divisible-by (-> test-str
-                         (str/split #" ")
-                         last
-                         Integer/parseInt)
-        monkey-if-true (-> tests-true-str
+(defn new-next-monkey [divisible-by tests-true-str tests-false-str]
+  (let [monkey-if-true (-> tests-true-str
                            (str/split #" ")
                            last
-                           Integer/parseInt)
+                           bigint)
         monkey-if-false (-> tests-false-str
                             (str/split #" ")
                             last
-                            Integer/parseInt)]
+                            bigint)]
     (fn [n]
       (if (zero? (mod n divisible-by))
         monkey-if-true
         monkey-if-false))))
 
 (defn parse-monkey [i s]
-  (let [[_ items operation test tests-true tests-false] (str/split s #"\n")]
-    {:inspected 0
+  (let [[_ items operation test tests-true tests-false] (str/split s #"\n")
+        divisible-by (-> test
+                         (str/split #" ")
+                         last
+                         bigint)]
+    {:divisible-by divisible-by
+     :inspected 0
      :items (->> (-> items
                      (str/split #":")
                      last
                      (str/split #","))
                  (map str/trim)
-                 (mapv #(Integer/parseInt %)))
-     :next-monkey (new-next-monkey test tests-true tests-false)
+                 (mapv bigint))
+     :next-monkey (new-next-monkey divisible-by tests-true tests-false)
      :num i
      :tx (new-tx operation)}))
 
@@ -56,11 +58,13 @@
        (map-indexed parse-monkey)
        (into [])))
 
+(def lcm
+  (->> init-state
+       (map :divisible-by)
+       (reduce *)))
+
 (defn item-turn [{from-monkey :num next-monkey :next-monkey tx :tx} state item]
-  (let [next-item (-> item
-                      tx
-                      (/ 3)
-                      long)
+  (let [next-item (-> item tx (mod lcm))
         to-monkey (next-monkey next-item)]
     (-> state
         (update-in [from-monkey :inspected] inc)
@@ -76,12 +80,17 @@
 (defn round [state]
   (reduce turn state (map :num state)))
 
+(defn rounds [n state]
+  (if (zero? n)
+    state
+    (do
+      (println n)
+      (recur (dec n)
+            (round state)))))
+
 (defn run []
   (->> init-state
-       (seq/successive round)
-       (drop 1)
-       (take 20)
-       last
+       (rounds 10000)
        (map :inspected)
        sort
        reverse
