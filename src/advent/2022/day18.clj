0549fc7cac7527af7b6d82d04d3243aaf2691eac
Dennis Hostetler <dlhostet@gmail.com>
2022-12-18 01:18:05 -0500

    2022, Day 18, Part 2.

--- src/advent/2022/day18.clj
+++ src/advent/2022/day18.clj
@@ -21,6 +21,38 @@
               offset [-1 1]]
           (update point i + offset))))
 
+(def min-x (->> cubes (map #(nth % 0)) (reduce min) dec))
+(def max-x (->> cubes (map #(nth % 0)) (reduce max) inc))
+(def min-y (->> cubes (map #(nth % 1)) (reduce min) dec))
+(def max-y (->> cubes (map #(nth % 1)) (reduce max) inc))
+(def min-z (->> cubes (map #(nth % 2)) (reduce min) dec))
+(def max-z (->> cubes (map #(nth % 2)) (reduce max) inc))
+
+(defn in-bounds? [[x y z]]
+  (and (<= min-x x max-x)
+       (<= min-y y max-y)
+       (<= min-z z max-z)))
+
+(defn valid-neighbors [point]
+  (->> point
+       neighbors
+       (filter in-bounds?)
+       (remove cubes)
+       (into #{})))
+
+(defn count-external-surfaces
+  ([] (count-external-surfaces 0 #{} (valid-neighbors [min-x min-y min-z])))
+  ([num-surfaces checked to-check]
+   (if (empty? to-check)
+     num-surfaces
+     (let [point (first to-check)
+           checked (conj checked point)]
+       (recur (+ num-surfaces (->> point neighbors (filter cubes) count))
+              checked
+              (-> to-check
+                  (disj point)
+                  (into (->> point valid-neighbors (remove checked)))))))))
+
 (defn run []
   (->> cubes
        (map neighbors)
