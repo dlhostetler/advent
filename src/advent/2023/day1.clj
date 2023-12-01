5b84281c40928eca2cfe3e1e1b29038ec64a9295
Dennis Hostetler <dlhostet@gmail.com>
2023-12-01 01:11:58 -0500

    2023, Day 1, Part 2.

--- src/advent/2023/day1.clj
+++ src/advent/2023/day1.clj
@@ -8,12 +8,44 @@
       slurp
       (str/split #"\n")))
 
-(defn digit? [c]
-  (Character/isDigit c))
+(def w-to-n
+  {"one" "1"
+   "two" "2"
+   "three" "3"
+   "four" "4"
+   "five" "5"
+   "six" "6"
+   "seven" "7"
+   "eight" "8"
+   "nine" "9"})
+
+(def digits (concat (keys w-to-n) (vals w-to-n)))
+
+(defn first-digit-at [s digit]
+  (when-let [i (str/index-of s digit)]
+    [i digit]))
+
+(defn last-digit-at [s digit]
+  (when-let [i (str/last-index-of s digit)]
+    [i digit]))
+
+(defn digit-by [s index-fn position-fn]
+  (->> (map #(index-fn s %) digits)
+       (remove nil?)
+       (sort-by first)
+       position-fn
+       last))
+
+(defn first-digit [s]
+  (let [digit (digit-by s first-digit-at first)]
+    (or (w-to-n digit) digit)))
+
+(defn last-digit [s]
+  (let [digit (digit-by s last-digit-at last)]
+    (or (w-to-n digit) digit)))
 
 (defn to-number [s]
-  (Integer/parseInt (str (->> s (filter digit?) first)
-                         (->> s (filter digit?) last))))
+  (Integer/parseInt (str (first-digit s) (last-digit s))))
 
 (defn run []
   (->> input
