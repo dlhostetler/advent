ad69c8191510ed5c3ac6f905de46d823f8644eb1
Dennis Hostetler <dlhostet@gmail.com>
2023-12-04 00:50:11 -0500

    2023, Day 4, Part 2.

--- src/advent/2023/day4.clj
+++ src/advent/2023/day4.clj
@@ -4,35 +4,37 @@
             [clojure.string :as str]
             [plumbing.core :refer :all]))
 
-(def input
-  (-> "resources/2023/day4.input"
-      io/reader
-      slurp
-      (str/split #"\n")))
-
 (defn parse-card [s]
   (let [[game all-nums] (str/split s #":")
         [winners nums] (str/split all-nums #"\|")]
-    {:id (Integer/parseInt (re-find #"\d+" game))
-     :nums (->> (str/split (str/trim nums) #" ")
-                (remove empty?)
-                (map str/trim)
-                (map #(Integer/parseInt %))
-                (into #{}))
-     :winners (->> (str/split (str/trim winners) #" ")
-                   (remove empty?)
-                   (map str/trim)
-                   (map #(Integer/parseInt %))
-                   (into #{}))}))
+    [(Integer/parseInt (re-find #"\d+" game))
+     {:nums (->> (str/split (str/trim nums) #" ")
+                 (remove empty?)
+                 (map str/trim)
+                 (map #(Integer/parseInt %))
+                 (into #{}))
+      :winners (->> (str/split (str/trim winners) #" ")
+                    (remove empty?)
+                    (map str/trim)
+                    (map #(Integer/parseInt %))
+                    (into #{}))}]))
 
-(defn ->score [{nums :nums winners :winners}]
-  (let [total-matches (count (set/intersection nums winners))]
-    (if (not (zero? total-matches))
-      (int (Math/pow 2 (dec total-matches)))
-      0)))
+(def cards
+  (->> (-> "resources/2023/day4.input"
+           io/reader
+           slurp
+           (str/split #"\n"))
+       (map parse-card)))
+
+(defn card->cards [cardsplosion [card-num {nums :nums winners :winners}]]
+  (let [total-matches (count (set/intersection nums winners))
+        new-cards (into {}
+                        (for [i (range total-matches)]
+                          [(+ (inc card-num) i)
+                           (get cardsplosion card-num)]))]
+    (merge-with + cardsplosion new-cards)))
 
 (defn run []
-  (->> input
-       (map parse-card)
-       (map ->score)
+  (->> (reduce card->cards (zipmap (map first cards) (repeat 1)) cards)
+       vals
        (reduce +)))
