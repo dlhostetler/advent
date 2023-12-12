38e46c6e2c8b4406f2ae90f950f3ec98dd94f61a
Dennis Hostetler <dlhostet@gmail.com>
2023-12-12 02:58:21 -0500

    2023, Day 12, Part 2.

--- src/advent/2023/day12.clj
+++ src/advent/2023/day12.clj
@@ -1,14 +1,20 @@
 (ns advent.2023.day12
   (:require [clojure.java.io :as io]
-            [clojure.set :as set]
             [clojure.string :as str]
             [plumbing.core :refer :all]))
+
+(def unfold 5)
 
 (defn parse-row [line]
   (let [[pattern groups] (str/split line #" ")]
     {:groups (->> (str/split groups #",")
-                  (mapv #(Integer/parseInt %)))
-     :pattern pattern}))
+                  (mapv #(Integer/parseInt %))
+                  (repeat unfold)
+                  flatten)
+     :pattern (str (->> pattern
+                        (repeat unfold)
+                        (str/join "?"))
+                   ".")}))
 
 (def rows
   (->> (-> "resources/2023/day12.input"
@@ -17,52 +23,63 @@
            (str/split #"\n"))
        (map parse-row)))
 
-(defn replace-at [s idx replacement]
-  (str (subs s 0 idx) replacement (subs s (inc idx))))
+(defn safe-nth [coll index]
+  (when (< index (count coll))
+    (nth coll index)))
 
-(defn valid? [pattern groups]
-  (let [counts (->> (str/split pattern #"[^#]+")
-                    (map count)
-                    (remove zero?))]
-    (and (= (count groups) (count counts))
-         (->> groups
-              (map = counts)
-              (every? true?)))))
+(declare count-arrangements)
 
-(defn invalid? [pattern groups]
-  (if (str/includes? pattern "?")
-    false
-    (not (valid? pattern groups))))
+(defn count-group [{pattern :pattern groups :groups :as row}]
+  (let [num-springs (first groups)]
+    (cond
+      (->> pattern (take num-springs) (some (partial = \.)))
+      0
 
-(declare ->arrangements)
+      (= (safe-nth pattern num-springs) \#)
+      0
 
-(defn try-replacement [{pattern :pattern groups :groups :as row} idx char arrangements]
-  (if (= (nth pattern idx) \?)
-    (let [next-pattern (replace-at pattern idx char)]
-      (if (invalid? next-pattern groups)
-        arrangements
-        (->arrangements (assoc row :pattern next-pattern)
-                        (inc idx)
-                        arrangements)))
-    (->arrangements row (inc idx) arrangements)))
+      :else
+      (count-arrangements (-> row
+                              (update :pattern subs (+ num-springs 1))
+                              (update :groups rest))))))
 
-(defn ->arrangements
-  ([row]
-   (->arrangements row 0 #{}))
-  ([{pattern :pattern groups :groups :as row} idx arrangements]
-   ;(println idx pattern)
-   (if (or (>= idx (count pattern))
-           (not (str/includes? pattern "?")))
-     (if (valid? pattern groups)
-       (conj arrangements pattern)
-       arrangements)
-     (set/union (try-replacement row idx \# arrangements)
-                (try-replacement row idx \. arrangements)))))
+(defn count-arrangements [{pattern :pattern groups :groups :as row}]
+  (cond
+    (and (zero? (count pattern))
+         (zero? (count groups)))
+    1
 
-(alter-var-root #'->arrangements memoize)
+    (zero? (count pattern))
+    0
+
+    (and (zero? (count groups))
+         (some (partial = \#) pattern))
+    0
+
+    (zero? (count groups))
+    1
+
+    (< (count pattern)
+       (+ (reduce + groups) (dec (count groups))))
+    0
+
+    (= (first pattern) \.)
+    (count-arrangements (update row :pattern subs 1))
+
+    (= (first pattern) \#)
+    (count-group row)
+
+    :else
+    (+ (count-arrangements (-> row
+                               (update :pattern subs 1)
+                               (update :pattern #(str "#" %))))
+       (count-arrangements (-> row
+                               (update :pattern subs 1)
+                               (update :pattern #(str "." %)))))))
+
+(alter-var-root #'count-arrangements memoize)
 
 (defn run []
   (->> rows
-       (pmap ->arrangements)
-       (map count)
+       (pmap count-arrangements)
        (reduce +)))
