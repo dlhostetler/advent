7e9793f58e1547c15b61f35e56eebd62a3035dab
Dennis Hostetler <dlhostet@gmail.com>
2023-12-21 03:30:14 -0500

    2023, Day 21, Part 2.

--- src/advent/2023/day21.clj
+++ src/advent/2023/day21.clj
@@ -10,9 +10,16 @@
 
 (def start-at (->> garden (filter (comp #(= \S %) last)) first first))
 
+(defn not-rock? [[x y]]
+  (not= \# (garden [(mod x (grid/max-x+1 garden))
+                    (mod y (grid/max-y+1 garden))])))
+
 (defn neighbor-plots [point]
-  (->> (grid/cardinal-neighbors garden point)
-       (filter #(not= \# (garden %)))))
+  (->> [(grid/north point)
+        (grid/east point)
+        (grid/south point)
+        (grid/west point)]
+       (filter not-rock?)))
 
 (defn step [plots point num-steps]
   (if (zero? num-steps)
@@ -22,7 +29,25 @@
 
 (alter-var-root #'step memoize)
 
-(defn run []
+(defn num-plots [num-steps]
   (let [plots (atom #{})]
-    (step plots start-at 64)
+    (step plots start-at num-steps)
     (count @plots)))
+
+(defn q [n [a0 a1 a2]]
+  (let [b0 a0
+        b1 (- a1 a0)
+        b2 (- a2 a1)]
+    (long (+ b0
+             (* b1 n)
+             (* (/ (* n (- n 1)) 2)
+                (- b2 b1))))))
+
+(defn run []
+  (let [step-goal 26501365
+        width (grid/max-x+1 garden)
+        n (int (mod step-goal width))]
+    (q (long (/ step-goal width))
+       [(num-plots n)
+        (num-plots (+ n width))
+        (num-plots (+ n (* 2 width)))])))
