f7d5d29a3d50d9984a27e1e595c023c28491d339
Dennis Hostetler <dlhostet@gmail.com>
2024-12-07 00:18:55 -0500

    2024, Day 7, Part 1.

---
+++ src/advent/2024/day7.clj
@@ -0,0 +1,31 @@
+(ns advent.2024.day7
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [plumbing.core :refer :all]))
+
+(defn parse-line [line]
+  (let [[target operands] (str/split line #":")]
+    {:operands (map #(Long/parseLong %) (str/split (str/trim operands) #" "))
+     :target (Long/parseLong target)}))
+
+(def input
+  (->> (-> "resources/2024/day7.input"
+           io/reader
+           slurp
+           (str/split #"\n"))
+       (map parse-line)))
+
+(defn valid? [target operands total]
+  (if (empty? operands)
+    (= total target)
+    (or (valid? target (rest operands) (+ total (first operands)))
+        (valid? target (rest operands) (* total (first operands))))))
+
+(defn valid-equation? [{:keys [operands target]}]
+  (valid? target (rest operands) (first operands)))
+
+(defn run []
+  (->> input
+       (filter valid-equation?)
+       (map :target)
+       (reduce +)))
