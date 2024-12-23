ed567044205be099ecc0742b50f3461d5731b749
Dennis Hostetler <dlhostet@gmail.com>
2024-12-23 01:03:40 -0500

    2024, Day 23, Part 2.

--- src/advent/2024/day23.clj
+++ src/advent/2024/day23.clj
@@ -1,7 +1,7 @@
 (ns advent.2024.day23
   (:require [clojure.java.io :as io]
-            [clojure.math.combinatorics :as combo]
             [clojure.string :as str]
+            [loom.alg :as graph.alg]
             [loom.graph :as graph]))
 
 (defn into-sets [sets [from to]]
@@ -18,15 +18,9 @@
 (def full-network
   (graph/graph edges))
 
-(defn maybe-chief-lan? [computers]
-  (some #(str/starts-with? % "t") computers))
-
 (defn run []
-  (->> (for [node (graph/nodes full-network)
-             [successor0 successor1] (-> (graph/successors full-network node)
-                                         (combo/combinations 2))
-             :when ((graph/successors full-network successor0) successor1)]
-         #{node successor0 successor1})
-       (into #{})
-       (filter maybe-chief-lan?)
-       count))
+  (->> (graph.alg/maximal-cliques full-network)
+       (sort-by count)
+       last
+       sort
+       (str/join ",")))
